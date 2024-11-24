ec51aec0fa7ccc9fc64ba402ae2c6a749ab7da3d
Zeno <zenowkc@gmail.com>
2024-11-24 13:28:49 -0800

    deployment setup

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/COMP-3170-Frontend_Web_Development_2-Assignment_07/',
 })
