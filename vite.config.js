fa9255de43eb9009e53aa8ceeaf443a8202bf881
Zeno <zenowkc@gmail.com>
2024-11-24 16:56:38 -0800

    final version 1.0.0

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/COMP-3170-Frontend_Web_Development_2-Assignment_07/',
+  base: '/COMP-3170-Frontend_Web_Development_2-Assignment_07/',
 })
